0b60b203d58d9559a52448100855c68df243ba40
Paul Massey <paul.massey@tenforce.com>
2016-10-26 09:41:32 +0200

    minor fixes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 #################################################################
 echo "*** Running Bootstrap.sh ****"
 
-apt-get install -y emacs dos2unix rasqal-utils git  vim
+apt-get install -y emacs dos2unix rasqal-utils git vim
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
@@ -15,7 +15,8 @@ systemctl status docker
 groupadd docker
 usermod -aG docker vagrant
 
-git clone https://github.com/binarin/docker-org-export.git /usr/local/docker-org-import
+# git clone https://github.com/binarin/docker-org-export.git /usr/local/docker-org-import
 
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
+
